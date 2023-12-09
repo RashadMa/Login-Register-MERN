ef1e72060b06edf03e79207a8129067afcf38cc4
rashadma <rashadkhm@code.edu.az>
2023-12-09 17:30:17 +0200

    NodeJS controller process

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+import User from "../models/user.models.js";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const newUser = new User({
+    username,
+    email,
+    password,
+  });
+  await newUser.save();
+  res.status(201).json({ message: "User created" });
+};
