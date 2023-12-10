61a55a2733ebc87efe523462cde7c23925ff58e6
rashadma <rashadkhm@code.edu.az>
2023-12-10 15:10:38 +0200

    controller progress and signup ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,18 @@
 import User from "../models/user.models.js";
+import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({
     username,
     email,
-    password,
+    password: hashedPassword,
   });
-  await newUser.save();
-  res.status(201).json({ message: "User created" });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created" });
+  } catch (error) {
+    next(error);
+  }
 };
