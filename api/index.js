207545ab7c643aac89a13ebdc389983e9b9131c7
rashadma <rashadkhm@code.edu.az>
2023-12-11 15:42:25 +0200

    test2

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("Server listening on port 3000");ß
+  console.log("Server listening on port 3000");
 });
 
 app.use("/api/user", userRoutes);
